64872e7c25e647f6ec3a048738439d4e98bef14f
kiethohoai <kietho.060193@gmail.com>
2024-09-12 03:29:59 +0700

    #12 Signup Page and Login Page UI Design

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,68 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    console.log('🚀🚀🚀data=', email, password);
+  };
+
   return (
-    <div>
-      <h1>LoginPage</h1>
+    <div className="h-screen w-full hero-bg">
+      <header className="max-w-6xl mx-auto flex items-center p-4">
+        <Link to={'/'}>
+          <img src="/netflix-logo.png" alt="logo" className="w-52" />
+        </Link>
+      </header>
+
+      <div className="flex justify-center items-center mt-20 mx-3">
+        <div className="w-full max-w-md p-8 space-y-6 bg-black/60 rounded-lg shadow-md">
+          <h1 className="text-center text-white text-2xl font-bold mb-4">Login</h1>
+          <form className="space-y-4" onSubmit={handleSignIn}>
+            <div>
+              <label htmlFor="email" className="text-sm font-medium text-gray-300 block">
+                Email
+              </label>
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                id="email"
+                placeholder="you@example.com"
+                className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="password" className="text-sm font-medium text-gray-300 block">
+                Password
+              </label>
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                id="password"
+                placeholder="*******"
+                className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
+              />
+            </div>
+
+            <button className="w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700">
+              Sign In
+            </button>
+          </form>
+
+          <div className="text-center text-gray-400">
+            Don't have an account?{'  '}
+            <Link to={'/signup'} className="text-red-500 hover:underline">
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

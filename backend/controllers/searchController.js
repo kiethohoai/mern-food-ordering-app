e89e97a05fa11e379d335926c5721b1493e57d21
kiethohoai <kietho.060193@gmail.com>
2024-09-11 16:40:29 +0700

    #10 Search Routes

--- backend/controllers/searchController.js
+++ backend/controllers/searchController.js
@@ -36,5 +36,115 @@ export const searchPerson = async (req, res) => {
     });
   }
 };
-export const searchMovie = async (req, res) => {};
-export const searchTv = async (req, res) => {};
+
+// todo searchMovie
+export const searchMovie = async (req, res) => {
+  const { query } = req.params;
+  try {
+    const respone = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`,
+    );
+
+    if (respone.results.length === 0) {
+      res.status(404).send(null);
+    }
+
+    await User.findByIdAndUpdate(req.user._id, {
+      $push: {
+        searchHistory: {
+          id: respone.results[0].id,
+          image: respone.results[0].poster_path,
+          title: respone.results[0].title,
+          searchType: 'movie',
+          createdAt: new Date(),
+        },
+      },
+    });
+
+    res.status(200).json({
+      success: true,
+      content: respone.results,
+    });
+  } catch (error) {
+    console.log('🚀Error on searchMovie Controller=', error.message);
+    res.status(500).json({
+      status: false,
+      message: 'Internal Server Error!',
+    });
+  }
+};
+
+export const searchTv = async (req, res) => {
+  const { query } = req.params;
+  try {
+    const respone = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/search/tv?query=${query}&include_adult=false&language=en-US&page=1`,
+    );
+
+    if (respone.results.length === 0) {
+      res.status(404).send(null);
+    }
+
+    await User.findByIdAndUpdate(req.user._id, {
+      $push: {
+        searchHistory: {
+          id: respone.results[0].id,
+          image: respone.results[0].poster_path,
+          title: respone.results[0].name,
+          searchType: 'tv',
+          createdAt: new Date(),
+        },
+      },
+    });
+
+    res.status(200).json({
+      success: true,
+      content: respone.results,
+    });
+  } catch (error) {
+    console.log('🚀Error on searchMovie Controller=', error.message);
+    res.status(500).json({
+      status: false,
+      message: 'Internal Server Error!',
+    });
+  }
+};
+
+export const getSearchHistory = async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      content: req.user.searchHistory,
+    });
+  } catch (error) {
+    console.log('🚀Error at getSearchHistory controller =', error);
+    res.status(500).json({
+      success: false,
+      message: 'Internal Server Error!',
+    });
+  }
+};
+
+export const removeItemFromSearchHistory = async (req, res) => {
+  let { id } = req.params;
+  id = parseInt(id);
+
+  try {
+    await User.findByIdAndUpdate(req.user._id, {
+      $pull: {
+        searchHistory: { id: id },
+      },
+    });
+
+    res.status(200).json({
+      success: true,
+      message: 'Item removed from search history!',
+    });
+  } catch (error) {
+    console.log('🚀Error at removeItemFromSearchHistory controller =', error);
+    res.status(500).json({
+      success: false,
+      message: 'Internal Server Error!',
+    });
+  }
+};
--- backend/routes/searchRoutes.js
+++ backend/routes/searchRoutes.js
@@ -1,10 +1,19 @@
 import express from 'express';
-import { searchMovie, searchPerson, searchTv } from '../controllers/searchController.js';
+import {
+  getSearchHistory,
+  removeItemFromSearchHistory,
+  searchMovie,
+  searchPerson,
+  searchTv,
+} from '../controllers/searchController.js';
 
 const router = express.Router();
 
 router.get('/person/:query', searchPerson);
 router.get('/movie/:query', searchMovie);
 router.get('/tv/:query', searchTv);
+
+router.get('/history', getSearchHistory);
+router.delete('/history/:id', removeItemFromSearchHistory);
 
 export default router;
